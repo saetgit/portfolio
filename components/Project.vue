28b0905034a8722d388cd5bd174623158ddc812a
saetgit <etemadian.sara.1998@gmail.com>
2024-06-29 18:39:49 +0330

    add editapi in server

--- components/Project.vue
+++ components/Project.vue
@@ -1,13 +1,32 @@
 <template>
     <div>
-    <p>This is Component Project</p>
+        <p>Project List</p>
+        <div class="bg-black" v-for="project in projects" :key="project.id">
+            <p>{{ project.name }}</p>
+        </div>
     </div>
 </template>
 
 <script setup>
+import { ref } from 'vue';
+import { useFetch } from '#imports';
 
+const projects = ref([]);
+
+const { data, error } = await useFetch('/api/projects');
+
+if (!error.value) {
+    projects.value = data.value;
+} else {
+    console.error(error.value);
+}
 </script>
 
 <style scoped>
-
+.bg-black {
+    background-color: black;
+    color: white;
+    padding: 10px;
+    margin: 5px 0;
+}
 </style>
---
+++ server/api/projects.js
@@ -0,0 +1,8 @@
+export default defineEventHandler(() => {
+    return [
+      { id: 1, name: 'Project Alpha' },
+      { id: 2, name: 'Project Beta' },
+      { id: 3, name: 'Project Gamma' }
+    ];
+  });
+  
