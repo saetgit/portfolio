7b5a5b66110dab4fdea424c13ec27f5e133d3df8
saetgit <etemadian.sara.1998@gmail.com>
2024-06-29 18:08:55 +0330

    add component slider

---
+++ components/Slider.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="flex justify-center h-10 ">
+        <div>
+            <p>I am sara etemadian</p>
+        </div>
+        <div><img src="../static/img/person-sara-etemadian.jpg" class="rounded-[50%] h-[200px] w-[200px]"
+                alt="person" /></div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <h1>Hello, Im sara etemadian.</h1>
+    <Slider/>
     </div>
 </template>
 
