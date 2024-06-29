c91be5de946e1194e708d1f1a836d6a3a2e5544b
saetgit <etemadian.sara.1998@gmail.com>
2024-06-29 18:18:40 +0330

    add edit slider

--- components/Slider.vue
+++ components/Slider.vue
@@ -1,7 +1,8 @@
 <template>
-    <div class="flex justify-center ">
-        <div>
+    <div class="flex justify-center p-5 items-center text-emerald-600">
+        <div class="pr-4">
             <p>I am sara etemadian</p>
+            <b>Front-end developer</b>
         </div>
         <div><img src="../static/img/person-sara-etemadian.jpg" class="rounded-[50%] h-[200px] w-[200px]"
                 alt="person" /></div>
