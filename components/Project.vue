fe32dc0d42880ad28a4794e7449e27b9bb6d60b4
saetgit <etemadian.sara.1998@gmail.com>
2024-06-30 15:17:10 +0330

    editadd postion and url in project component

--- components/Project.vue
+++ components/Project.vue
@@ -1,12 +1,15 @@
 <template>
     <div class="mb-[100px]">
-        <div>
-            <p class="text-xl font-bold mb-4">Project List</p>
+        <div class="text-center mb-8">
+            <p class="text-2xl font-semibold text-gray-800">Project List</p>
         </div>
         <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-5">
             <div class="bg-emerald-600 text-white p-5 rounded shadow hover:shadow-lg transform hover:translate-y-1 transition-transform duration-300"
                 v-for="project in projects" :key="project.id">
-                <p>{{ project.name }}</p>
+                <a :href="project.url" target="_blank">
+                    <p class="text-[14px]">{{ project.name }}</p>
+                </a>
+                <p class="text-[10px]">{{ project.position }}</p>
             </div>
         </div>
     </div>
--- server/api/projects.js
+++ server/api/projects.js
@@ -1,11 +1,11 @@
 export default defineEventHandler(() => {
   return [
-    { id: 1, name: 'Asnaf Panel', postion: 'Front-End Developer', url: 'http://davari.nkh-asnaf.ir:5006/auth/login/' },
-    { id: 2, name: 'Car exhibition CRM', postion: 'Front-End Developer', url: 'https://crm.ahmad-gholami.ir/' },
-    { id: 3, name: 'Sazadeh engineering office software', postion: 'Front-End Developer', url: 'https://sazandeh.app/' },
-    { id: 4, name: 'Id Product', postion: 'Full-stack Developer', url: '#' },
-    { id: 5, name: 'Malima platform', postion: 'Front-End Developer', url: 'https://panel.malima.ir/' },
-    { id: 6, name: 'Research week virtual exhibition system', postion: 'Front-End Developer', url: '#' },
+    { id: 1, name: 'Asnaf Panel', position: 'Front-End Developer', url: 'http://davari.nkh-asnaf.ir:5006/auth/login/' },
+    { id: 2, name: 'Car exhibition CRM', position: 'Front-End Developer', url: 'https://crm.ahmad-gholami.ir/' },
+    { id: 3, name: 'Sazadeh engineering office software', position: 'Front-End Developer', url: 'https://sazandeh.app/' },
+    { id: 4, name: 'Id Product', position: 'Full-stack Developer', url: '#' },
+    { id: 5, name: 'Malima platform', position: 'Front-End Developer', url: 'https://panel.malima.ir/' },
+    { id: 6, name: 'Research week virtual exhibition system', position: 'Front-End Developer', url: '#' },
 
   ];
 });
