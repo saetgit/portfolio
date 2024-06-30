ad33539103ee037ab4b5098d22be876924d0c860
saetgit <etemadian.sara.1998@gmail.com>
2024-06-30 15:22:03 +0330

    add skill.js in api server and create skill components

---
+++ server/api/skills.js
@@ -0,0 +1,17 @@
+export default defineEventHandler(() => {
+    return [
+      { id: 1, name: 'Vue.js', },
+      { id: 2, name: 'Nuxt.js', },
+      { id: 3, name: 'Html5', },
+      { id: 4, name: 'CSS', },
+      { id: 5, name: 'Bootstrap', },
+      { id: 6, name: 'Bootstrap Vue', },
+      { id: 7, name: 'Tailwind', },
+      { id: 8, name: 'JavaScript', },
+      { id: 9, name: 'TypeScript', },
+      { id: 10, name: 'Tailwind', },
+      { id: 11, name: 'GitHub', },
+      { id: 12, name: 'GitLab', },
+      { id: 13, name: 'Linux', },
+    ];
+  });
