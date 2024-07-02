b600dbf049b2af1cad04e1ed348ae051bff7f949
saetgit <etemadian.sara.1998@gmail.com>
2024-07-02 16:58:24 +0330

    add button download resume slider

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <nav class="bg-emerald-600 p-4 text-white fixed w-full">
+        <nav class="bg-emerald-600 p-4 text-white fixed w-full z-10">
             <button @click="toggleMenu" class="block md:hidden">
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
                     stroke="currentColor">
--- components/Slider.vue
+++ components/Slider.vue
@@ -3,9 +3,17 @@
         <div class="pr-4">
             <p>I am sara etemadian</p>
             <b>Front-end developer</b>
+            <div class="mt-4">
+                <a href="static/doc/Sara_Etemadian_v1.pdf"
+                    class="inline-block px-6 py-3 bg-black text-white no-underline rounded transition-transform transform hover:bg-gray-800 hover:scale-105 shadow-md hover:shadow-lg">
+                    Download Resume
+                </a>
+            </div>
+
         </div>
-        <div><img src="../static/img/person-sara-etemadian.jpg" class="rounded-[50%] h-[200px] w-[200px]"
-                alt="person" /></div>
+        <div>
+            <img src="../static/img/person-sara-etemadian.jpg" class="rounded-[50%] h-[200px] w-[200px]" alt="person" />
+        </div>
     </div>
 </template>
 
