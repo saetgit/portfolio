1bb1c76424963a33891126a7b857785e211ccce8
saetgit <etemadian.sara.1998@gmail.com>
2024-07-06 17:32:00 +0330

    config nuxt

--- components/Slider.vue
+++ components/Slider.vue
@@ -1,18 +1,18 @@
 <template>
     <div class="flex justify-center px-5 items-center text-emerald-600 pt-[80px] mb-8">
-        <div class="pr-4">
+        <div class="pr-[100px]">
             <p>I am sara etemadian</p>
             <b>Front-end developer</b>
             <div class="mt-4">
                 <a :href="pdfLink" download
-                    class="inline-block px-6 py-3 bg-black text-white no-underline rounded transition-transform transform hover:bg-gray-800 hover:scale-105 shadow-md hover:shadow-lg">
+                    class="inline-block px-3 py-3 bg-black text-white no-underline rounded transition-transform transform hover:bg-gray-800 hover:scale-105 shadow-md hover:shadow-lg">
                     Download Resume
                 </a>
             </div>
 
         </div>
         <div>
-            <img src="../static/img/person-sara-etemadian.jpg" class="rounded-[50%] h-[200px] w-[200px]" alt="person" />
+            <img src="../static/img/person-sara-etemadian.jpg" class="rounded-[50%]  w-[200px]" alt="person" />
         </div>
     </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  
   modules: ["@nuxtjs/tailwindcss", "@nuxtjs/tailwindcss"],
+
+
+  target: 'static', // برای ساختن یک سایت استاتیک
+  build: {
+    publicPath: 'git@github.com:saetgit/portfolio.git'
+  }
 });
